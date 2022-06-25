7c8bfce51798e55a9e8614d7fe26f9dfa91af4af
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-25 10:06:21 +0430

    edit BaseEntity and User and Wallet

--- practice-one/practice-one/src/main/java/com/jpaAndHibernateTutorial/base/domain/BaseEntity.java
+++ practice-one/practice-one/src/main/java/com/jpaAndHibernateTutorial/base/domain/BaseEntity.java
@@ -5,7 +5,7 @@ import javax.persistence.Id;
 import javax.persistence.MappedSuperclass;
 import java.io.Serializable;
 
-// @MappedSuperclass : Each class that inherits the abc class implements or maps the fields in that class in the database.
+// @MappedSuperclass : Each class that inherits the BaseEntity class implements or maps the fields in that class in the database.
 @MappedSuperclass
 public abstract class BaseEntity<ID extends Serializable> implements Serializable {
     // Serializable : Marker transition
--- practice-one/practice-one/src/main/java/com/jpaAndHibernateTutorial/domain/Wallet.java
+++ practice-one/practice-one/src/main/java/com/jpaAndHibernateTutorial/domain/Wallet.java
@@ -1,14 +1,15 @@
 package com.jpaAndHibernateTutorial.domain;
 
-import javax.persistence.*;
+import com.jpaAndHibernateTutorial.base.domain.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
 
 @Entity
 @Table(name = Wallet.TABLE_NAME)
-public class Wallet {
+public class Wallet extends BaseEntity<Long> {
     public static final String TABLE_NAME = "wallet";
-    @Id
-    @GeneratedValue
-    private Long id;
     private Long totalAmount;
     private Long cashAmount;
     private Long creditAmount;
@@ -24,20 +25,11 @@ public class Wallet {
     }
 
     public Wallet(Long id, Long totalAmount, Long cashAmount, Long creditAmount) {
-        this.id = id;
+        super(id);
         this.totalAmount = totalAmount;
         this.cashAmount = cashAmount;
         this.creditAmount = creditAmount;
     }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
     public Long getTotalAmount() {
         return totalAmount;
     }
@@ -73,10 +65,10 @@ public class Wallet {
     @Override
     public String toString() {
         return "Wallet{" +
-                "id=" + id +
-                ", totalAmount=" + totalAmount +
+                "totalAmount=" + totalAmount +
                 ", cashAmount=" + cashAmount +
                 ", creditAmount=" + creditAmount +
+                ", user=" + user +
                 '}';
     }
 }
