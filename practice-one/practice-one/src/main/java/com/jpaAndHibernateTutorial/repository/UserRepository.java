a9a3459e1a7421c4b3ce26830380a62ae0c5bf8b
Dragon2623 <armin1.aliani1@gmail.com>
2022-06-29 18:13:00 +0430

    edit interface UserRepository and class UserRepositoryImpl

--- practice-one/practice-one/src/main/java/com/jpaAndHibernateTutorial/repository/UserRepository.java
+++ practice-one/practice-one/src/main/java/com/jpaAndHibernateTutorial/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.jpaAndHibernateTutorial.repository;
 
 import com.jpaAndHibernateTutorial.base.repository.BaseRepository;
 import com.jpaAndHibernateTutorial.domain.User;
+import com.jpaAndHibernateTutorial.service.dto.UserBriefProjection;
+
+import java.util.List;
 
 public interface UserRepository extends BaseRepository<User, Long> {
     User findByUsername(String username);
+
+    List<UserBriefProjection> getUserBriefProjection();
 }
--- practice-one/practice-one/src/main/java/com/jpaAndHibernateTutorial/repository/impl/UserRepositoryImpl.java
+++ practice-one/practice-one/src/main/java/com/jpaAndHibernateTutorial/repository/impl/UserRepositoryImpl.java
@@ -3,8 +3,10 @@ package com.jpaAndHibernateTutorial.repository.impl;
 import com.jpaAndHibernateTutorial.base.repository.impl.BaseRepositoryImpl;
 import com.jpaAndHibernateTutorial.domain.User;
 import com.jpaAndHibernateTutorial.repository.UserRepository;
+import com.jpaAndHibernateTutorial.service.dto.UserBriefProjection;
 
 import javax.persistence.EntityManager;
+import java.util.List;
 
 public class UserRepositoryImpl extends BaseRepositoryImpl<User, Long> implements UserRepository {
 
@@ -25,6 +27,17 @@ public class UserRepositoryImpl extends BaseRepositoryImpl<User, Long> implement
     @Override
     public User findByUsername(String username) {
         return entityManager.createQuery("select u from User u where u.username=:username", User.class).setParameter("username", username).getSingleResult();
+    }
+
+    @Override
+    public List<UserBriefProjection> getUserBriefProjection() {
+
+        return entityManager.createQuery(
+                "select new com.jpaAndHibernateTutorial.service.dto." +
+                        "UserBriefProjection(u.id, u.firstName, u.lastName) from User u",
+                UserBriefProjection.class
+        ).getResultList();
+
     }
 
 }
